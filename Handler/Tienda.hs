3d79ccdeb49a76689947f659b6a6b956da427a7e
Escuela Colombiana de Ingenieria <ferchogarc010@gmail.com>
2015-03-22 17:57:19 -0500

    cambios crear y detalles

---
+++ Handler/Tienda.hs
@@ -0,0 +1,34 @@
+module Handler.Tienda where
+
+import Import
+import Yesod.Form.Bootstrap3 (bfs, renderBootstrap3 , BootstrapFormLayout(..) )
+import Yesod.Text.Markdown (markdownField)
+ 
+tiendaForm :: AForm Handler Tienda
+tiendaForm = Tienda
+          <$> areq textField (bfs ("nombre"::Text)) Nothing 
+          <*> areq markdownField (bfs ("descripcion"::Text)) Nothing
+          <*> areq doubleField (bfs ("precio"::Text)) Nothing
+
+getTiendaNewR :: Handler Html
+getTiendaNewR = do
+       (widget, encoding) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm tiendaForm
+       defaultLayout $ do
+           $(widgetFile "tienda/new") 
+
+postTiendaNewR :: Handler Html
+postTiendaNewR = do
+       ((result, widget), encoding) <- runFormPost $ renderBootstrap3 BootstrapBasicForm tiendaForm
+       case result of
+            FormSuccess tienda -> do
+                                  tId <- runDB $ insert tienda
+                                  redirect (TiendaR tId)
+            _ -> defaultLayout $ do
+                      $(widgetFile "tienda/new")
+
+getTiendaR :: TiendaId -> Handler Html
+getTiendaR tId = do
+   tienda <- runDB $ get404 tId
+   defaultLayout $ do
+        $(widgetFile "tienda/details")           
+          
--- Handler/TiendaNew.hs
+++
@@ -1,9 +0,0 @@
-module Handler.TiendaNew where
-
-import Import
-
-getTiendaNewR :: Handler Html
-getTiendaNewR = error "Not yet implemented: getTiendaNewR"
-
-postTiendaNewR :: Handler Html
-postTiendaNewR = error "Not yet implemented: postTiendaNewR"
